94df9b94772ca8c6c8912088eb05fca34a9e6562
Roja1912 <roja.t@accionlabs.com>
2019-02-14 14:57:33 +0530

    adding custom query logic.

--- utils/countcheck.py
+++ utils/countcheck.py
@@ -1,17 +1,32 @@
 arr = []
 
 
-def count_check(source_cursor, target_cursor, source_table, target_table):
+def count_check(source_cursor, target_cursor, source_table, target_table, test_query):
     try:
-        cursor = source_cursor
-        cursor.execute('SELECT COUNT(*) FROM {}'.format(source_table))
+        print("query", test_query)
 
+        lst=test_query.strip('“').strip('”').split(';')
+        print(lst)
+        cursor = source_cursor
+        if test_query == 'None':
+           print("in if")
+           cursor.execute('SELECT COUNT(*) FROM {}'.format(source_table))
+           pass
+        else:
+            cursor.execute(lst[0])
+
+            print("in else")
+            pass
         for row in cursor:
             for src_count in row:
                 pass
 
         cursor1 = target_cursor
-        cursor1.execute('SELECT COUNT(*) FROM {}'.format(target_table))
+        if test_query == 'None':
+            cursor1.execute('SELECT COUNT(*) FROM {}'.format(target_table))
+        else:
+            cursor1.execute(lst[1])
+            pass
         for row in cursor1:
             for target_count in row:
                 pass
