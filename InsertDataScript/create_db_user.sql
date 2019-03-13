e93ddf6ee5959c681f834056269c3a7653c0a340
Harshad Kavathiya <harshad.kavathiya@rackspace.com>
2019-03-13 12:54:48 +0530

    Added User creation script

---
+++ InsertDataScript/create_db_user.sql
@@ -0,0 +1,6 @@
+CREATE USER 'acciom_user'@'localhost' IDENTIFIED BY 'acciom_password’;
+GRANT ALL PRIVILEGES ON *.* TO 'acciom_user'@'localhost';
+CREATE DATABASE acciom_db;
+CREATE DATABASE source_db;
+CREATE DATABASE dest_db;
+FLUSH PRIVILEGES;
--- db_config.py
+++ db_config.py
@@ -1,7 +1,7 @@
-SOURCE_DB_USERNAME = 'Acciom_user'
-SOURCE_DB_PASSWORD = 'Acciomuser'
+SOURCE_DB_USERNAME = 'acciom_user'
+SOURCE_DB_PASSWORD = 'acciom_password'
 SOURCE_DB_HOSTNAME = 'localhost'
 
-DEST_DB_USERNAME = 'Acciom_user1'
-DEST_DB_PASSWORD = 'Acciomuser'
+DEST_DB_USERNAME = 'acciom_user'
+DEST_DB_PASSWORD = 'acciom_password'
 DEST_DB_HOSTNAME = 'localhost'
