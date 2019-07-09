dfd20063afc91b89856ca96836f4f0ee652acb3e
Reachakhil <bhardwaj.akhil96@gmail.com>
2019-07-09 13:17:49 +0530

    custom qry in datavalidation problem.

--- application/api/testsuite.py
+++ application/api/testsuite.py
@@ -119,15 +119,24 @@ class TestSuites(Resource):
                 else:
                     if ";" in p:
                         query_split = p.split(";")
+                        print(query_split)
+
                         final = [a.split(":") for a in query_split]
+                        print(final)
                         query["sourceqry"] = final[0][1] if 'srcqry' in final[
                             0] else ""
                         query["targetqry"] = final[1][1] if 'targetqry' in \
                                                             final[1] else ""
 
                     else:
-                        q = p.strip("targetqry:")
-                        query["targetqry"] = q
+                        if "srcqry:" in p.lower():
+                            q = p.strip("srcqry:")
+                            query["sourceqry"] = q
+                            query['targetqry'] = ""
+                        else:
+                            q = p.strip("targetqry:")
+                            query["targetqry"] = q
+                            query['sourceqry'] = ""
 
                 jsondict = {"column": column, "table": table, "query": query}
                 # print("141", jsondict)
